8a327c70791e01455b538ffe707b2fa78c85cb0d
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2023-04-02 14:46:06 +0200

    indent option

--- egctools/egctools.py
+++ egctools/egctools.py
@@ -128,162 +128,165 @@ def lines_index(fname):
     del lines_idx['S_or_T']
   return lines, lines_idx
 
-def _extract_V_or_C(rule_rt, rule_id, lines, lines_idx, skip, indent,
+def _extract_V_or_C(rule_rt, rule_id, lines, lines_idx, skip, indent, indented,
                     follow_G, exclude_G_id, follow_A, exclude_A_id, follow_ST):
   results = [indent + lines[lines_idx[rule_rt][rule_id]['line']]]
   skip.append(rule_id)
-  indent1 = indent+"  "
+  indent1 = indent + "  " if indented else ""
   if follow_G:
     for group_id in lines_idx[rule_rt][rule_id]['refs']['G']:
       if group_id != exclude_G_id and group_id not in skip:
         results.extend(_extract_G(group_id, lines, lines_idx, skip,
-                                  indent1, True, False, exclude_G_id, False))
+                      indent1, indented, True, False, exclude_G_id, False))
   if follow_A:
     for attr_id in lines_idx[rule_rt][rule_id]['refs']['A']:
       if attr_id != exclude_A_id and attr_id not in skip:
         results.extend(_extract_A(attr_id, lines, lines_idx, skip,
-                                  indent1, True, None, False))
+                                  indent1, indented, True, None, False))
   if follow_ST:
     for source_rt in ['S', 'T']:
       for source_id in lines_idx[rule_rt][rule_id]['refs'][source_rt]:
         if source_id not in skip:
           results.extend(_extract_S_or_T(source_rt, source_id, lines, lines_idx,
-            skip, indent1, True, False))
+            skip, indent1, indented, True, False))
   return results
 
 def _extract_S_or_T(source_rt, source_id, lines, lines_idx, skip, indent,
-                    follow_D, climb_VC):
+                    indented, follow_D, climb_VC):
   uline = lines[lines_idx[source_rt][source_id]['line']]
   skip.append(source_id)
   results = [indent + uline]
   line = parsed_line(uline)
   document_id_dt = SPEC["external_resource::external_resource_link"]
   document_id = document_id_dt.encode(line['document_id'])
-  indent1 = indent + "  "
+  indent1 = indent + "  " if indented else ""
   if follow_D:
     if document_id not in skip:
       results.extend(_extract_D(document_id, lines, lines_idx, skip,
-                                indent1, False))
+                                indent1, indented, False))
   if climb_VC:
     for rt in ['V', 'C']:
       for line_id in lines_idx[source_rt][source_id]['ref_by'][rt]:
         if line_id not in skip:
           results.extend(_extract_V_or_C(rt, line_id, lines, lines_idx, skip,
-            indent1, True, None, True, None, False))
+            indent1, indented, True, None, True, None, False))
   return results
 
-def _extract_A(attr_id, lines, lines_idx, skip, indent,
+def _extract_A(attr_id, lines, lines_idx, skip, indent, indented,
                follow_U, exclude_U_id, climb_VC):
   results = [indent + lines[lines_idx['A'][attr_id]['line']]]
   skip.append(attr_id)
-  indent1 = indent + "  "
+  indent1 = indent + "  " if indented else ""
   if follow_U:
     for unit_id in lines_idx['A'][attr_id]['refs']['U']:
       if unit_id != exclude_U_id and unit_id not in skip:
         results.extend(_extract_U(unit_id, lines, lines_idx, skip, indent1,
-                                  True, False, exclude_U_id, True, False))
+                          indented, True, False, exclude_U_id, True, False))
   if climb_VC:
     for rt in ['V', 'C']:
       for line_id in lines_idx['A'][attr_id]['ref_by'][rt]:
         if line_id not in skip:
           results.extend(_extract_V_or_C(rt, line_id, lines, lines_idx, skip,
-            indent1, True, None, True, attr_id, True))
+            indent1, indented, True, None, True, attr_id, True))
   return results
 
-def _extract_recursively(rt, line_id, lines, lines_idx, skip, indent,
+def _extract_recursively(rt, line_id, lines, lines_idx, skip, indent, indented,
                          follow_M, exclude_id, direction):
   results = []
-  stack = [(ln, indent+"  ") for ln in lines_idx[rt][line_id][direction][rt]]
+  stack = [(ln, indent) for ln in lines_idx[rt][line_id][direction][rt]]
   while len(stack) > 0:
-    line2_id, indent2 = stack.pop()
+    line2_id, indent1 = stack.pop()
     if line2_id not in skip:
-      results.append(indent2 + lines[lines_idx[rt][line2_id]['line']])
+      results.append(indent1 + lines[lines_idx[rt][line2_id]['line']])
       skip.append(line2_id)
+      indent2 = indent1 + "  " if indented else ""
       if follow_M:
         for model_lineno in lines_idx[rt][line2_id][direction]['M']:
-          results.append(indent2 + "  " + lines[model_lineno])
+          results.append(indent2 + lines[model_lineno])
       for line3_id in lines_idx[rt][line2_id][direction][rt]:
         if line3_id not in skip and line3_id != exclude_id:
-          stack.append((line3_id, indent2 + "  "))
+          stack.append((line3_id, indent2))
   return results
 
-def _extract_U(unit_id, lines, lines_idx, skip, indent,
+def _extract_U(unit_id, lines, lines_idx, skip, indent, indented,
                follow_U, climb_U, exclude_U_id, follow_M, climb_A):
   results = [indent + lines[lines_idx['U'][unit_id]['line']]]
+  indent1 = indent + "  " if indented else ""
   skip.append(unit_id)
   if follow_M:
     for model_lineno in lines_idx['U'][unit_id]['refs']['M']:
-      results.append(indent + "  " + lines[model_lineno])
+      results.append(indent1 + lines[model_lineno])
   if follow_U:
     results.extend(_extract_recursively('U', unit_id, lines, lines_idx, skip,
-                     indent, follow_M, exclude_U_id, 'refs'))
+                     indent1, indented, follow_M, exclude_U_id, 'refs'))
   if climb_U:
     results.extend(_extract_recursively('U', unit_id, lines, lines_idx, skip,
-                     indent, follow_M, exclude_U_id, 'ref_by'))
+                     indent1, indented, follow_M, exclude_U_id, 'ref_by'))
   if climb_A:
     for attr_id in lines_idx['U'][unit_id]['ref_by']['A']:
       if attr_id not in skip:
-        results.extend(_extract_A(attr_id, lines, lines_idx, skip, indent+"  ",
-          True, unit_id, True))
+        results.extend(_extract_A(attr_id, lines, lines_idx, skip, indent1,
+          indented, True, unit_id, True))
   return results
 
-def _extract_G(group_id, lines, lines_idx, skip, indent,
+def _extract_G(group_id, lines, lines_idx, skip, indent, indented,
                follow_G, climb_G, exclude_G_id, climb_VC):
   results = [indent + lines[lines_idx['G'][group_id]['line']]]
   skip.append(group_id)
-  indent1 = indent + "  "
+  indent1 = indent + "  " if indented else ""
   if follow_G:
     results.extend(_extract_recursively('G', group_id, lines, lines_idx, skip,
-                     indent1, False, exclude_G_id, 'refs'))
+                     indent1, indented, False, exclude_G_id, 'refs'))
   if climb_G:
     results.extend(_extract_recursively('G', group_id, lines, lines_idx, skip,
-                     indent1, False, exclude_G_id, 'ref_by'))
+                     indent1, indented, False, exclude_G_id, 'ref_by'))
   if climb_VC:
     for rt in ['V', 'C']:
       for line_id in lines_idx['G'][group_id]['ref_by'][rt]:
         if line_id not in skip:
           results.extend(_extract_V_or_C(rt, line_id, lines, lines_idx, skip,
-            indent1, False, group_id, True, None, True))
+            indent1, indented, False, group_id, True, None, True))
   return results
 
-def _extract_D(document_id, lines, lines_idx, skip, indent, climb_ST):
+def _extract_D(document_id, lines, lines_idx, skip, indent, indented, climb_ST):
   results = [indent + lines[lines_idx['D'][document_id]['line']]]
   skip.append(document_id)
+  indent1 = indent + "  " if indented else ""
   if climb_ST:
     for source_rt in ['S', 'T']:
       for source_id in lines_idx['D'][document_id]['ref_by'][source_rt]:
         if source_id not in skip:
           results += _extract_S_or_T(source_rt, source_id, lines,
-              lines_idx, skip, indent + "  ", False, True)
+              lines_idx, skip, indent1, indented, False, True)
   return results
 
-def extract(line_id, fname):
+def extract(line_id, fname, indented):
   lines, lines_idx = lines_index(fname)
   if line_id in lines_idx['D']:
     return _extract_D(line_id, lines, lines_idx, [],
-                      "", True)
+                      "", indented, True)
   elif line_id in lines_idx['S']:
     return _extract_S_or_T('S', line_id, lines, lines_idx, [],
-                           "", True, True)
+                           "", indented, True, True)
   elif line_id in lines_idx['T']:
     return _extract_S_or_T('T', line_id, lines, lines_idx, [],
-                           "", True, True)
+                           "", indented, True, True)
   elif line_id in lines_idx['G']:
     return _extract_G(line_id, lines, lines_idx, [],
-                      "", True, True, None, True)
+                      "", indented, True, True, None, True)
   elif line_id in lines_idx['A']:
     return _extract_A(line_id, lines, lines_idx, [],
-                      "", True, None, True)
+                      "", indented, True, None, True)
   elif line_id in lines_idx['U']:
     return _extract_U(line_id, lines, lines_idx, [],
-                      "", True, True, line_id, True,
+                      "", indented, True, True, line_id, True,
         True)
   elif line_id in lines_idx['V']:
     return _extract_V_or_C('V', line_id, lines, lines_idx, [],
-                           "", True, None, True, None, True)
+                           "", indented, True, None, True, None, True)
   elif line_id in lines_idx['C']:
     return _extract_V_or_C('C', line_id, lines, lines_idx, [],
-                           "", True, None, True, None, True)
+                           "", indented, True, None, True, None, True)
   raise ValueError("Unknown line ID: " + line_id)
 
 def statistics(fname, stats = None):
